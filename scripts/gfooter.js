11cd821a2caf95a59575f8863cc02ecc50f08479
Snehal Sonawane <sonawane@Snehals-MacBook-Pro.local>
2024-06-20 22:47:36 +0530

    combining customConfig and consumerConfig and footer url

--- scripts/gfooter.js
+++ scripts/gfooter.js
@@ -4,8 +4,7 @@ const consumerConfig = {
     myConfig: 'Snehal from consumer site',
     imsClientId: 'milo', //provide consumer client id
     miloLibs: 'https://main--milo--adobecom.hlx.page/libs',
+    nonMiloFooterUrl: 'https://main--federal--adobecom.hlx.page/drafts/snehal/footer',
 }
 
 GlobalFooter(document.querySelector("footer"), consumerConfig);
-
-console.log(GlobalFooter);
