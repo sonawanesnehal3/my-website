899766fd6ecbf7c9fb87813cb352d46ad42fc525
Snehal Sonawane <sonawane@Snehals-MacBook-Pro.local>
2024-06-20 10:51:27 +0530

    Adding consumer config placeholder

--- scripts/gfooter.js
+++ scripts/gfooter.js
@@ -1,5 +1,11 @@
 import { GlobalFooter } from '/static/libs/global-footer/global-footer.js';
 
-GlobalFooter(document.querySelector("footer"));
+const consumerConfig = {
+    myConfig: 'Snehal from consumer site',
+    imsClientId: 'milo', //provide consumer client id
+    miloLibs: 'https://main--milo--adobecom.hlx.page/libs',
+}
+
+GlobalFooter(document.querySelector("footer"), consumerConfig);
 
 console.log(GlobalFooter);
