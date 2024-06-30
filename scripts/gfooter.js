0c31373a264b57f49f565ef3c01b0ec7c761b682
Snehal Sonawane <sonawane@Snehals-MacBook-Pro.local>
2024-06-30 16:12:11 +0530

    importing stagefootercss branch

--- scripts/gfooter.js
+++ scripts/gfooter.js
@@ -1,8 +1,8 @@
-import { GlobalFooter } from 'https://stagefooter--milo--sonawanesnehal3.hlx.page/libs/deps/global-footer/global-footer.js';
+import { GlobalFooter } from 'https://stagefootercss--milo--sonawanesnehal3.hlx.page/libs/deps/global-footer/global-footer.js';
 
 const consumerConfig = {
     imsClientId: 'milo', // Consumer can pass {consumerimsClientId}, it will take precedence over milo imsClientId
-    miloLibs: 'https://stagefooter--milo--sonawanesnehal3.hlx.page/libs', //Consumer can pass {consumermiloLibs}, it will take precedence over miloLibs
+    miloLibs: 'https://stagefootercss--milo--sonawanesnehal3.hlx.page/libs', //Consumer can pass {consumermiloLibs}, it will take precedence over miloLibs
     nonMiloFooterUrl: 'https://main--federal--adobecom.hlx.page/drafts/snehal/footer',
     nonMiloRoot: 'https://main--milo--adobecom.hlx.page' //Consumer contentRoot to fetch placeholder Ex. 'https://www.stage.adobe.com/cc-shared' 
 }
